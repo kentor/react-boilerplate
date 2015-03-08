2da8d489fe8d8e5013549d6fb6e354c17489de41
Kenneth Chung <ken70r@gmail.com>
2015-03-07 22:55:21 -0800

    Use .jsx extension for files that uses jsx.

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 var Route = require('react-router').Route;
 var Router = require('react-router');
 
-var Index = require('./components/index');
+var Index = require('./components/Index.jsx');
 
 Router.run((
   <Route name="app" path="/" handler={Index}>
