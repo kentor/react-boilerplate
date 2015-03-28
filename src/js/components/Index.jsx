37e0695ac0bf70e119766475d4edd02db95683fc
Kenneth Chung <ken70r@gmail.com>
2015-03-28 02:05:32 -0700

    Update npm and react

--- src/js/components/Index.jsx
+++ src/js/components/Index.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 
 var Index = React.createClass({
   render() {
-    return <h1>{`${Immutable.Range(0, 10).toArray()}`}</h1>;
+    return <h1>{Immutable.Range(0, 10)}</h1>;
   },
 });
 
