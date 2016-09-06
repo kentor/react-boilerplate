d08d7f24a5b04780fe282158f12bf3ab35cb0a7c
Kenneth Chung <kenneth@rescale.com>
2016-09-05 17:51:00 -0700

    wip

--- src/js/components/Root.js
+++ src/js/components/Root.js
@@ -1,102 +1,165 @@
+const Icon = require('./Icon');
 const React = require('react');
 
 const Root = React.createClass({
   render() {
     return (
-      <main className="ph5 pv4 mw8 center">
-        <h1 className="fw5 f2 tc ma0">Kenneth Chung</h1>
-        <p className="tc mv3">
-          Software Engineer specializing in Front End Development
-        </p>
-        <p className="tc ma0">
-          1368 20th Ave. San Francisco, CA 94122
-          {' • '}
-          <a href="mailto:ken70r@gmail.com">ken70r@gmail.com</a>
-          {' • '}
-          (415) 690-9363
-        </p>
-        <p className="tc ma0">
-          <a href="https://github.com/kentor">https://github.com/kentor</a>
-          {' • '}
-          <a href="http://kentor.me">http://kentor.me</a>
-        </p>
+      <main
+        className="relative dark-gray"
+        style={{ height: '11in', width: '8.5in' }}
+      >
+        <div className="bg-dark-gray white pv3 ph4 f6 lh-solid flex justify-between">
+          <div>
+            {/*
+            <Icon icon="email" size="1em" />
+            */}
+            <a href="mailto:ken70r@gmail.com" className="no-underline">
+              ken70r@gmail.com
+            </a>
+          </div>
+          <div>
+            {/*
+            <Icon icon="location" size="1em" />
+            */}
+            <span>San Francisco, CA</span>
+          </div>
+          <div>
+            {/*
+            <Icon icon="phone" size="1em" />
+            */}
+            <span>415-690-9363</span>
+          </div>
+          <div>
+            {/*
+            <Icon icon="site" size="1em" />
+            */}
+            <a href="https://kentor.me" className="no-underline">
+              kentor.me
+            </a>
+          </div>
+          <div>
+            {/*
+            <Icon icon="github" size="1em" />
+            */}
+            <a href="https://github.com/kentor" className="no-underline">
+              github.com/kentor
+            </a>
+          </div>
+        </div>
 
-        <h2 className="f3 mt4 mb3">Work Experience</h2>
+        <section className="pa4">
+          <h1 className="fw6 f-2 ma0 relative">
+            Kenneth Chung
+          </h1>
+          <p className="mt1 mb3">
+            Software Engineer specializing in Frontend Development
+          </p>
 
-        <span className="f4">
-          <strong>Rescale, San Francisco, CA</strong>
+          <h2 className="f3 mt4 mb3 fw5">
+            <span className="dib">Work Experience</span>
+          </h2>
+
+          <span className="f4">
+            <span className="fw6">Rescale, San Francisco, CA</span>
+            {' '}
+            Lead Frontend Engineer (October 2014 — present)
+          </span>
+          <ul className="pl3 lh-title">
+            <li className="mv2">
+              Introduced the React ecosystem and modern web development tooling
+              to the development team. This includes Babel, Browserify, ESLint,
+              Gulp, Hot Module Reloading, and NPM.
+            </li>
+            <li className="mv2">
+              Developed i18n supported administrative and management
+              applications for our enterprise users and software partners using
+              Django, React, Redux, and Immutable.js.
+            </li>
+            <li className="mv2">
+              Maintain the frontend of our main PaaS product which is built on
+              Angular, Django, Django Rest Framework.
+            </li>
+            <li className="mv2">
+              Implemented the S3/Azure HTML5 file uploader with client-side
+              encryption using WebCrypto.
+            </li>
+            <li className="mv2">
+              Implemented a fully responsive marketing site with the Tachyons
+              CSS library.
+            </li>
+          </ul>
+
+          <span className="f4">
+            <span className="fw6">
+              Shop It To Me, San Francisco, CA</span>
+            {' '}
+            Software Engineer (October 2012 — August 2014)
+          </span>
           {' '}
-          <em>Software Engineer (October 2014 — present)</em>
-        </span>
-        <ul className="pl3">
-          <li className="mv2 lh-title">
-            Developed consumer facing and i18n supported administrative and
-            management web applications with React, Redux, D3, and Immutable.js.
-          </li>
-          <li className="mv2 lh-title">
-            Implemented an HTML5 file uploader with client-side encryption using
-            WebCrypto.
-          </li>
-          <li className="mv2 lh-title">
-            Maintain our main platform product which is built on Angular,
-            Django, Django Rest Framework.
-          </li>
-          <li className="mv2 lh-title">
-            Implemented a fully responsive marketing site with Tachyons.
-          </li>
-          <li className="mv2 lh-title">
-            Introduced the React ecosystem and modern web development tooling to
-            the development team. This includes Browserify, Hot Module
-            Reloading, Babel, ESLint, and Gulp.
-          </li>
-        </ul>
+          <ul className="pl3 lh-title">
+            <li className="mv2">
+              Developed SPAs for the core Shop It To Me shopping service using
+              Ember, Ruby, and Ruby on Rails for both the desktop web and mobile
+              web platforms.
+            </li>
+            <li className="mv2">
+              Created and maintained the JSON API using Rails, MySQL, and
+              Sphinx.
+            </li>
+            <li className="mv2">
+              Developed the password-less, email based authentication system
+              used in our iOS app.
+            </li>
+            <li className="mv2">
+              Developed the price drop email alert system and implemented the
+              accompanying iOS notifications.
+            </li>
+            <li className="mv2">
+              Maintained cross browser compatibility for all major browsers,
+              including IE8+.
+            </li>
+          </ul>
 
-        <span className="f4">
-          <strong>Shop It To Me, San Francisco, CA</strong>
-          {' '}
-          <em>Software Engineer (October 2012 — August 2014)</em>
-        </span>
-        {' '}
-        <ul className="pl3">
-          <li className="mv2 lh-title">
-            Developed an ambitious, UI rich application for the core Shop It To
-            Me shopping service using Ember, Ruby, and Ruby on Rails for both
-            the desktop web and mobile web platforms, serving our 4 million+
-            members.
-          </li>
-          <li className="mv2 lh-title">
-            Created and maintained the JSON API for Shop It To Me mobile web and
-            iOS app using Rails, MySQL, and Sphinx.
-          </li>
-          <li className="mv2 lh-title">
-            Developed the password-less, email based authentication system used
-            in our iOS app.
-          </li>
-          <li className="mv2 lh-title">
-            Developed the price drop email alert system and implemented the
-            accompanying iOS notifications.
-          </li>
-          <li className="mv2 lh-title">
-            Maintained cross browser compatibility of our website for all major
-            browsers, including IE8+.
-          </li>
-          <li className="mv2 lh-title">
-            Discussed good coding practices with colleagues and stressed the
-            SOLID principles during code reviews and in my own code.
-          </li>
-        </ul>
+          <h2 className="f3 mt4 mb3 fw5">
+            <span className="dib">Open Source</span>
+          </h2>
+          <ul className="pl3 lh-title">
+            <li className="mv2 lh-title">
+              <a href="https://github.com/kentor/react-click-outside">
+                react-click-outside
+              </a>:
+              {' '}
+              A higher order component for providing click outside detection.
+            </li>
+            <li className="mv2 lh-title">
+              <a href="https://github.com/kentor/kentor.me">
+                kentor.me
+              </a>:
+              {' '}
+              My website and blog, statically generated using React and Node.js
+              APIs with live reloading.
+            </li>
+          </ul>
 
-        <h2 className="f3 mt4 mb3">Technical Skills</h2>
-        <p className="mv3 lh-title">
-          Javascript, ES6, React, Angular, Ember, D3, Ruby, Ruby on Rails,
-          Python, Django, Django Rest Framework, PostgreSQL, MySQL
-        </p>
+          <h2 className="f3 mt4 mb3 fw5">
+            <span className="dib">Technical Skills</span>
+          </h2>
+          <p className="mv3 lh-title">
+            Javascript, ES6, React, Angular, Ember, HTML, CSS, Ruby, Ruby on
+            Rails, Python, Django, SQL
+          </p>
 
-        <h2 className="f3 mt4 mb3">Education</h2>
-        <p className="mv3">
-          Bachelor of Science, Chemistry, University of California, Berkeley
-          — GPA: 3.85
-        </p>
+          <h2 className="f3 mt4 mb3 fw5">
+            <span className="dib">Education</span>
+          </h2>
+          <p className="mv3">
+            Bachelor of Science, Chemistry, University of California, Berkeley
+            — GPA: 3.85
+          </p>
+          <p>
+            Selected coursework: Data Structures and Algorithms (CS 61B) — A+
+          </p>
+        </section>
       </main>
     );
   },
