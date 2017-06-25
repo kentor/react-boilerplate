b3ac2820f747d860e20c68dfc6487ed8b0e799a3
Kenneth Chung <ken70r@gmail.com>
2017-06-24 20:01:02 -0700

    Use React Router v4
    Will work better with the new hot reloader

--- src/js/router.js
+++ src/js/router.js
@@ -1,9 +1,9 @@
 const React = require('react');
 const Root = require('./components/Root');
-const { browserHistory, Route, Router } = require('react-router');
+const { BrowserRouter } = require('react-router-dom');
 
 module.exports = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Root} />
-  </Router>
+  <BrowserRouter>
+    <Root />
+  </BrowserRouter>
 );
